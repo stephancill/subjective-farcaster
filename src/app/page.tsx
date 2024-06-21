3695a02ccc577182298e5d112ec7fc80b3066ddf
Stephan Cilliers <5469870+stephancill@users.noreply.github.com>
2024-06-22 01:22:54 +0200

    fix: remove frame from layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import { fetchMetadata } from "frames.js/next";
+import { APP_URL } from "../lib/const";
+
+export async function generateMetadata() {
+  const frameMetadata = await fetchMetadata(`${APP_URL}/frames`);
+  return {
+    title: "Subjective Farcaster",
+    description:
+      "Reaction and follow counts on farcaster based on an FID's wider network.",
+    other: {
+      ...frameMetadata,
+    },
+  };
+}
+
 export default function Home() {
   return (
     <div>
